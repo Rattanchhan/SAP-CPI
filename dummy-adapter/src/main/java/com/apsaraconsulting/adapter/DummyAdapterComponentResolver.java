55dad618a94074ec0a4cff4979e1ace5f0e14cd7
ratana <chhanratta92000@gmail.com>
2025-07-13 23:56:01 +0700

    using new camel version 3.14.7 with OSGI avoid blueprint any more

--- dummy-adapter/src/main/java/com/apsaraconsulting/adapter/DummyAdapterComponentResolver.java
+++ dummy-adapter/src/main/java/com/apsaraconsulting/adapter/DummyAdapterComponentResolver.java
@@ -1,19 +1,19 @@
-package com.apsaraconsulting.adapter;
-
-import org.apache.camel.CamelContext;
-import org.apache.camel.Component;
-import org.apache.camel.spi.ComponentResolver;
-
-/**
- * Component Resolver for Dummy Adapter Component
- */
-public class DummyAdapterComponentResolver implements ComponentResolver {
-
-    @Override
-    public Component resolveComponent(String name, CamelContext context) throws Exception {
-        if ("dummy-adapter".equals(name)) {
-            return new DummyAdapterComponent();
-        }
-        return null;
-    }
-}
+//package com.apsaraconsulting.adapter;
+//
+//import org.apache.camel.CamelContext;
+//import org.apache.camel.Component;
+//import org.apache.camel.spi.ComponentResolver;
+//
+///**
+// * Component Resolver for Dummy Adapter Component
+// */
+//public class DummyAdapterComponentResolver implements ComponentResolver {
+//
+//    @Override
+//    public Component resolveComponent(String name, CamelContext context) throws Exception {
+//        if ("dummy-adapter".equals(name)) {
+//            return new DummyAdapterComponent();
+//        }
+//        return null;
+//    }
+//}
